7212ed55cfa891b2407016c80c4ac834bf350907
robbinhan <luckyhanbin@gmail.com>
2014-05-15 21:36:48 +0800

    fix login coursera fix  get_content_site function

--- coursera-downloader.rb
+++ coursera-downloader.rb
@@ -4,6 +4,7 @@ require "uri"
 require 'net/http'
 require 'cgi'
 require 'nokogiri'
+require 'awesome_print'
 
 
 if ARGV.size < 3
@@ -20,17 +21,14 @@ def initial_response(uri)
 end
 
 def do_login(initial_response, username, password)
-  uri = URI('https://class.coursera.org/progfun-2012-001')
+  uri = URI('https://accounts.coursera.org/api/v1/login')
   cookies = ""
-
   Net::HTTP.start(uri.host, uri.port,:use_ssl => uri.scheme == 'https') do |http|
     request = Net::HTTP::Post.new(uri)
     request['Cookie'] = "csrftoken=#{initial_response["Set-Cookie"].split(";")[0].split("=")[1]}"
     request["X-CSRFToken"] = initial_response["Set-Cookie"].split(";")[0].split("=")[1]
     request["Referer"] = "https://accounts.coursera.org/signin"
-
     request.set_form_data('email' => username, 'password' => password)
-
     response = http.request(request)
     cookies = response["set-cookie"]
   end
@@ -39,12 +37,13 @@ def do_login(initial_response, username, password)
 end
 
 def build_cookie_string(cookies)
+  cookies = CGI::Cookie.parse(cookies)
   cauth = cookies["CAUTH"]
-  return "maestro_login_flag=1;CAUTH=#{cauth}"
+  return "maestro_login_flag=1;#{cauth}"
 end
 
 def course_content_uri(course_name)
- URI("https://class.coursera.org/#{course_name}/lecture/index")
+ URI("https://class.coursera.org/#{course_name}/lecture")
 end
 
 
@@ -54,8 +53,8 @@ def get_content_site(cookie_string, course_name)
   Net::HTTP.start(uri.host, uri.port,:use_ssl => uri.scheme == 'https') do |http|
     request = Net::HTTP::Get.new(uri)
     request["Cookie"] = cookie_string
-
-    data = http.request(request).body
+    response = http.request(request)
+    ap data = response.body
   end
 
   return data
@@ -77,61 +76,8 @@ end
 username = ARGV[0]
 password = ARGV[1]
 course_name = ARGV[2]
-
 initial_resp = initial_response(course_uri(course_name))
 cookies = do_login(initial_resp, username, password)
 cookie_string = build_cookie_string(cookies)
-
 data = get_content_site(cookie_string, course_name)
 links = get_download_links(data)
-
-agent = Mechanize.new
-# Download all files to the current directory
-links.each do |link|
-  unless (link=~ URI::regexp).nil?
-    uri = link
-    if (uri =~ /\.mp4/) || (uri =~ /srt/) || (uri =~ /\.pdf/) || (uri =~ /\.pptx/)
-      begin
-        head = agent.head(uri)
-      rescue Mechanize::ResponseCodeError => exception
-        if exception.response_code == '403'
-          filename = URI.decode(exception.page.filename).gsub(/.*filename=\"(.*)\"+?.*/, '\1')
-        else
-          raise exception # Some other error, re-raise
-        end
-      else
-        # First try to access direct the content-disposition header, because mechanize
-        # split the file at "/" and "\" and only use the last part. So we get trouble
-        # with "/" in filename.
-        if not head.response["Content-Disposition"].nil?
-          content_disposition = Mechanize::HTTP::ContentDispositionParser.parse head.response["Content-Disposition"]
-          filename = content_disposition.filename if content_disposition
-        end
-
-        # If we have no file found in the content disposition take the head filename
-        filename ||= head.filename
-        filename = URI.decode(filename.gsub(/http.*\/\//,""))
-      end
-
-      # Replace unwanted characters from the filename
-      filename = filename.gsub(":","").gsub("_","").gsub("/","_")
-
-      if File.exists?(filename)
-        p "Skipping #{filename} as it already exists"
-      else
-        p "Downloading #{uri} to #{filename}..."
-        begin
-          gotten = agent.get(uri)
-          gotten.save(filename)
-          p "Finished"
-        rescue Mechanize::ResponseCodeError => exception
-          if exception.response_code == '403'
-            p "Failed to download #{filename} for #{exception}"
-          else
-            raise exception # Some other error, re-raise
-          end
-        end
-      end
-    end
-  end
-end
